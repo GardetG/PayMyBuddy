2ce47d10fc302c978cff716be0a2795c56860fc5
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2021-12-26 12:06:27 +0100

    Add model class User

---
+++ src/main/java/com/openclassrooms/paymybuddy/model/User.java
@@ -0,0 +1,41 @@
+package com.openclassrooms.paymybuddy.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Model Class of an user with personals information like firstname and lastname and credentials
+ * information with email and password.
+ */
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  private int userId;
+
+  @Column(name = "firstname")
+  private String firstName;
+
+  @Column(name = "lastname")
+  private String lastName;
+
+  @Column(name = "email", unique = true)
+  private String email;
+
+  @Column(name = "password")
+  private String password;
+
+}
+
+
