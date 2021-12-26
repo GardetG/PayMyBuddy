61051acee673470e67dd769c4cc8ece5190ec421
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2021-12-26 12:06:04 +0100

    Add schema.sql and data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (email, password, firstname, lastname)
+  values ('user@mail.com', 'password', 'test', 'test');
+INSERT INTO user (email, password, firstname, lastname)
+    values ('user2@mail.com', 'password', 'test2', 'test2');
