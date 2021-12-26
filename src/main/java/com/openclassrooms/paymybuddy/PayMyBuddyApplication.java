2856699383dafc70f29af5e63b59b294a656bba9
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2021-12-26 12:49:30 +0100

    Add UserRepository Interface

--- src/main/java/com/openclassrooms/paymybuddy/PayMyBuddyApplication.java
+++ src/main/java/com/openclassrooms/paymybuddy/PayMyBuddyApplication.java
@@ -3,11 +3,14 @@ package com.openclassrooms.paymybuddy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Class of Pay My Buddy project.
+ */
 @SpringBootApplication
 public class PayMyBuddyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PayMyBuddyApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(PayMyBuddyApplication.class, args);
+  }
 
 }
---
+++ src/main/java/com/openclassrooms/paymybuddy/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.paymybuddy.repository;
+
+import com.openclassrooms.paymybuddy.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository Class for CRUD operations on users in database.
+ */
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
