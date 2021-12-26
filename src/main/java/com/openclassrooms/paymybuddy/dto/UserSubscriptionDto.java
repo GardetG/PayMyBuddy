0c4f4cfd6daf3f707e016dee961179e1dbfa86b0
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2021-12-26 19:04:10 +0100

    Add UserSubscriptionDto

---
+++ src/main/java/com/openclassrooms/paymybuddy/dto/UserSubscriptionDto.java
@@ -0,0 +1,29 @@
+package com.openclassrooms.paymybuddy.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * DTO Class for user subscription with firstname, lastname, email and password.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserSubscriptionDto {
+
+  @NotBlank(message = "Firstname is mandatory")
+  private String firstname;
+  @NotBlank(message = "Lastname is mandatory")
+  private String lastname;
+  @NotBlank(message = "Email is mandatory")
+  @Email(message = "Email is not a valid email address")
+  private String email;
+  @NotBlank(message = "Password is mandatory")
+  @Size(min = 8, message = "Password should have at least 8 characters")
+  private String password;
+
+}
