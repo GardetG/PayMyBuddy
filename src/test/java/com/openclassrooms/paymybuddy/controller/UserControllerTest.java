680676f70d2eb5d9648e8624ff0cbda0fb098bd0
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2021-12-31 17:19:31 +0100

    Add UserController POST update() unit tests

--- src/test/java/com/openclassrooms/paymybuddy/controller/UserControllerTest.java
+++ src/test/java/com/openclassrooms/paymybuddy/controller/UserControllerTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -42,6 +43,8 @@ class UserControllerTest {
 
   @Captor
   ArgumentCaptor<UserSubscriptionDto> subscriptionCaptor;
+  @Captor
+  ArgumentCaptor<UserInfoDto> infoCaptor;
 
   private UserInfoDto userInfoDto;
 
@@ -144,6 +147,88 @@ class UserControllerTest {
         .andExpect(jsonPath("$.firstname", is("Firstname is mandatory")))
         .andExpect(jsonPath("$.email", is("Email should be a valid email address")))
         .andExpect(jsonPath("$.password", is("Password should have at least 8 characters")));
+    verify(userService, times(0)).subscribe(any(UserSubscriptionDto.class));
+  }
+
+  @Test
+  void putUpdateTest() throws Exception {
+    // GIVEN
+    UserInfoDto updateDto = new UserInfoDto(1,"update", "test", "update@mail.com", BigDecimal.ZERO);
+    when(userService.update(any(UserInfoDto.class))).thenReturn(updateDto);
+
+    // WHEN
+    mockMvc.perform(put("/users")
+            .with(csrf())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(JsonParser.asString(updateDto)))
+
+        // THEN
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.userId", is(1)))
+        .andExpect(jsonPath("$.firstname", is("update")))
+        .andExpect(jsonPath("$.lastname", is("test")))
+        .andExpect(jsonPath("$.email", is("update@mail.com")))
+        .andExpect(jsonPath("$.wallet", is(0)));
+    verify(userService, times(1)).update(infoCaptor.capture());
+    assertThat(infoCaptor.getValue()).usingRecursiveComparison().isEqualTo(updateDto);
+
+  }
+
+  @Test
+  void putUpdateWithAlreadyUsedEmailTest() throws Exception {
+    // GIVEN
+    UserInfoDto updateDto = new UserInfoDto(1,"update", "test", "existing@mail.com", BigDecimal.ZERO);
+    when(userService.update(any(UserInfoDto.class))).thenThrow(
+        new EmailAlreadyExistsException("This email is already used"));
+
+    // WHEN
+    mockMvc.perform(put("/users")
+            .with(csrf())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(JsonParser.asString(updateDto)))
+
+        // THEN
+        .andExpect(status().isConflict())
+        .andExpect(jsonPath("$", is("This email is already used")));
+    verify(userService, times(1)).update(infoCaptor.capture());
+    assertThat(infoCaptor.getValue()).usingRecursiveComparison().isEqualTo(updateDto);
+  }
+
+  @Test
+  void putUpdateWhenNotFoundTest() throws Exception {
+    // GIVEN
+    UserInfoDto updateDto = new UserInfoDto(2,"update", "test", "update@mail.com", BigDecimal.ZERO);
+    when(userService.update(any(UserInfoDto.class))).thenThrow(
+        new ResourceNotFoundException("This user is not found"));
+
+    // WHEN
+    mockMvc.perform(put("/users")
+            .with(csrf())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(JsonParser.asString(updateDto)))
+
+        // THEN
+        .andExpect(status().isNotFound())
+        .andExpect(jsonPath("$", is("This user is not found")));
+    verify(userService, times(1)).update(infoCaptor.capture());
+    assertThat(infoCaptor.getValue()).usingRecursiveComparison().isEqualTo(updateDto);
+  }
+
+  @Test
+  void putInvalidUpdateTest() throws Exception {
+    // GIVEN
+    UserInfoDto invalidUpdateDto = new UserInfoDto(1,"","test", "testmail.com", BigDecimal.ZERO);
+
+    // WHEN
+    mockMvc.perform(put("/users")
+            .with(csrf())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(JsonParser.asString(invalidUpdateDto)))
+
+        // THEN
+        .andExpect(status().isUnprocessableEntity())
+        .andExpect(jsonPath("$.firstname", is("Firstname is mandatory")))
+        .andExpect(jsonPath("$.email", is("Email should be a valid email address")));
     verify(userService, times(0)).subscribe(any(UserSubscriptionDto.class));
   }
 }
