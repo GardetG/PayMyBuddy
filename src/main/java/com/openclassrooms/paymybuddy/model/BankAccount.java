413088ff1fde41ce12caf6e1b6133ae18ba55426
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-01 17:36:27 +0100

    Add model class BankAccount

---
+++ src/main/java/com/openclassrooms/paymybuddy/model/BankAccount.java
@@ -0,0 +1,49 @@
+package com.openclassrooms.paymybuddy.model;
+
+import java.math.BigDecimal;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Model Class of a bank account with tile, iban, bic and owner user oh the account.
+ */
+@Entity
+@Table(name = "bank_account")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankAccount {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "bank_account_id")
+  private int bankAccountId;
+
+  @Column(name = "title",  length = 60)
+  private String title;
+
+  @Column(name = "iban", length = 34)
+  private String iban;
+
+  @Column(name = "bic",  length = 11)
+  private String bic;
+
+  @Column(name = "balance")
+  private BigDecimal balance;
+
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "user_id")
+  private User user;
+}
