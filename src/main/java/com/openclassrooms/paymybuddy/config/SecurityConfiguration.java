742ceb8126b5e567256b1f0d165cac697fc15e19
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-02 10:32:25 +0100

    Add BCrypt passwordEncoder to SecurityConfiguration

--- src/main/java/com/openclassrooms/paymybuddy/config/SecurityConfiguration.java
+++ src/main/java/com/openclassrooms/paymybuddy/config/SecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.openclassrooms.paymybuddy.config;
 
 import com.openclassrooms.paymybuddy.service.CredentialsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Spring Security configuration.
@@ -23,7 +26,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   // be in a database, LDAP or in memory.
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(credentialsService);
+    auth.userDetailsService(credentialsService)
+        .passwordEncoder(passwordEncoder());
   }
 
   @Override
@@ -33,8 +37,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .antMatchers("/subscribe").permitAll()
         .anyRequest().fullyAuthenticated().and()
         .httpBasic().and()
-        .csrf().disable();
+        .csrf().disable().cors().disable();
 
   }
 
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
