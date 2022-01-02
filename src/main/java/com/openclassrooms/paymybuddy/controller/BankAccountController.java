c9e28cc843a38f02be2fcb904b2436193bfb5f58
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-02 23:24:12 +0100

    Implement BankAccountController addToUser() method

--- src/main/java/com/openclassrooms/paymybuddy/controller/BankAccountController.java
+++ src/main/java/com/openclassrooms/paymybuddy/controller/BankAccountController.java
@@ -4,9 +4,11 @@ import com.openclassrooms.paymybuddy.dto.BankAccountDto;
 import com.openclassrooms.paymybuddy.exception.ResourceNotFoundException;
 import com.openclassrooms.paymybuddy.service.BankAccountService;
 import java.util.List;
+import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -14,6 +16,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * Controller Class for managing user bank account.
@@ -44,6 +48,28 @@ public class BankAccountController {
 
     LOGGER.info("Response: List of user bank accounts sent");
     return ResponseEntity.ok(bankAccounts);
+  }
+
+  /**
+   * Handle HTTP POST request on user bank accounts.
+   *
+   * @param id of user
+   * @param bankAccount to add
+   * @return HTTP 201
+   * @throws ResourceNotFoundException when user not found
+   */
+  @PreAuthorize("hasRole('ADMIN') or #id == authentication.principal.userId")
+  @PostMapping("/users/{id}/bankaccounts")
+  public ResponseEntity<List<BankAccountDto>> deleteUser(@PathVariable int id, @Valid @RequestBody
+      BankAccountDto bankAccount)
+      throws ResourceNotFoundException {
+
+    LOGGER.info("Request: Add user {} new bank account", id);
+    List<BankAccountDto> bankAccounts = bankAccountService.addToUserId(id, bankAccount);
+
+    LOGGER.info("Response: User bank account added");
+    return ResponseEntity.status(HttpStatus.CREATED).body(bankAccounts);
+
   }
 
   /**
