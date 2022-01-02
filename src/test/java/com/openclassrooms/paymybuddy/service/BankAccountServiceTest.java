60575ffccec04078c73b626fa270c6a432163104
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-02 21:14:26 +0100

    Add BankAccountService addToUserId() unit tests

--- src/test/java/com/openclassrooms/paymybuddy/service/BankAccountServiceTest.java
+++ src/test/java/com/openclassrooms/paymybuddy/service/BankAccountServiceTest.java
@@ -43,8 +43,6 @@ class BankAccountServiceTest {
   @BeforeEach
   void setUp() {
     userTest = new User(1,"test","test","test@mail.com","12345678", BigDecimal.ZERO, new Role(1,"USER"), new HashSet<>());
-    userTest.getBankAccounts().add(new BankAccount(1,"PrimaryAccount", "1234567890abcdefghijklmnopqrstu123","12345678abc",BigDecimal.valueOf(100)));
-    userTest.getBankAccounts().add(new BankAccount(1,"PrimaryAccount", "1234567890abcdefghijklmnopqrstu456","12345678def",BigDecimal.valueOf(150)));
     account1DtoTest = new BankAccountDto(1, "PrimaryAccount","XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX123","XXXXXXXXabc");
     account2DtoTest = new BankAccountDto(1, "PrimaryAccount","XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX456","XXXXXXXXdef");
   }
@@ -52,6 +50,8 @@ class BankAccountServiceTest {
   @Test
   void getAllByUserIdTest() throws Exception {
     // GIVEN
+    userTest.getBankAccounts().add(new BankAccount(1,"PrimaryAccount", "1234567890abcdefghijklmnopqrstu123","12345678abc",BigDecimal.valueOf(100)));
+    userTest.getBankAccounts().add(new BankAccount(1,"PrimaryAccount", "1234567890abcdefghijklmnopqrstu456","12345678def",BigDecimal.valueOf(150)));
     when(userRepository.findById(anyInt())).thenReturn(Optional.of(userTest));
 
     // WHEN
@@ -65,7 +65,6 @@ class BankAccountServiceTest {
   @Test
   void getAllByUserIdWhenEmptyTest() throws Exception {
     // GIVEN
-    userTest.setBankAccounts(new HashSet<>());
     when(userRepository.findById(anyInt())).thenReturn(Optional.of(userTest));
 
     // WHEN
@@ -89,4 +88,39 @@ class BankAccountServiceTest {
         .hasMessageContaining("This user is not found");
     verify(userRepository, times(1)).findById(2);
   }
+
+  @Test
+  void addToUserIdTest() throws Exception {
+    // GIVEN
+    BankAccountDto accountToAddDto = new BankAccountDto(1, "PrimaryAccount","1234567890abcdefghijklmnopqrstu123","12345678xyz");
+    User updateUserTest = new User(1,"test","test","test@mail.com","12345678", BigDecimal.ZERO, new Role(1,"USER"), new HashSet<>());
+    updateUserTest.getBankAccounts().add(new BankAccount(1,"PrimaryAccount", "1234567890abcdefghijklmnopqrstu123","12345678abc",BigDecimal.valueOf(100)));
+    when(userRepository.findById(anyInt())).thenReturn(Optional.of(userTest));
+    when(userRepository.save(any(User.class))).thenReturn(updateUserTest);
+
+    // WHEN
+    List<BankAccountDto> actualListBankAccountDto = bankAccountService.addToUserId(1,accountToAddDto);
+
+    // THEN
+    assertThat(actualListBankAccountDto).usingRecursiveComparison().isEqualTo(List.of(account1DtoTest));
+    verify(userRepository, times(1)).findById(1);
+    verify(userRepository,times(1)).save(any(User.class));
+  }
+
+  @Test
+  void addToUserIdWhenNotFoundTest() {
+    // GIVEN
+    BankAccountDto accountToAddDto = new BankAccountDto(0, "PrimaryAccount","1234567890abcedfghijklmnopqrst789","12345678xyz");
+    when(userRepository.findById(anyInt())).thenReturn(Optional.empty());
+
+    // WHEN
+    assertThatThrownBy(() -> bankAccountService.addToUserId(2,accountToAddDto))
+
+        // THEN
+        .isInstanceOf(ResourceNotFoundException.class)
+        .hasMessageContaining("This user is not found");
+    verify(userRepository, times(1)).findById(2);
+    verify(userRepository,times(0)).save(any(User.class));
+  }
+
 }
