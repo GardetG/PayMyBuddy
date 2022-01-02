e3f4060e7f7b0b32a13622feca1e483ce44b250a
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-02 10:16:55 +0100

    Update SecurityConfiguration

--- src/main/java/com/openclassrooms/paymybuddy/config/SecurityConfiguration.java
+++ src/main/java/com/openclassrooms/paymybuddy/config/SecurityConfiguration.java
@@ -1,6 +1,9 @@
 package com.openclassrooms.paymybuddy.config;
 
+import com.openclassrooms.paymybuddy.service.CredentialsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,9 +15,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+  @Autowired
+  CredentialsService credentialsService;
+
+  // This method is for overriding the default AuthenticationManagerBuilder.
+  // We can specify how the user details are kept in the application. It may
+  // be in a database, LDAP or in memory.
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(credentialsService);
+  }
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
+        .authorizeRequests()
+        .antMatchers("/subscribe").permitAll()
+        .anyRequest().fullyAuthenticated().and()
+        .httpBasic().and()
         .csrf().disable();
 
   }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO user (email, password, firstname, lastname, wallet)
-  values ('user@mail.com', 'password', 'test', 'test', 50);
+  values ('user@mail.com', '{noop}password', 'test', 'test', 50);
 INSERT INTO user (email, password, firstname, lastname, wallet)
-    values ('user2@mail.com', 'password', 'test2', 'test2', 100);
+    values ('user2@mail.com', '{noop}password', 'test2', 'test2', 100);
