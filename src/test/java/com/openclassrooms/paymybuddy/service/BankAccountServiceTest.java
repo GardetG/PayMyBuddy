945a4e7ecce7143d85eece6f4499406ff680f255
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-02 22:09:16 +0100

    Add BankAccountService deleteById() unit tests

--- src/test/java/com/openclassrooms/paymybuddy/service/BankAccountServiceTest.java
+++ src/test/java/com/openclassrooms/paymybuddy/service/BankAccountServiceTest.java
@@ -9,7 +9,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.openclassrooms.paymybuddy.dto.BankAccountDto;
-import com.openclassrooms.paymybuddy.dto.UserInfoDto;
 import com.openclassrooms.paymybuddy.exception.ResourceNotFoundException;
 import com.openclassrooms.paymybuddy.model.BankAccount;
 import com.openclassrooms.paymybuddy.model.Role;
@@ -17,7 +16,6 @@ import com.openclassrooms.paymybuddy.model.User;
 import com.openclassrooms.paymybuddy.repository.UserRepository;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
@@ -38,33 +36,31 @@ class BankAccountServiceTest {
 
   private User userTest;
   private BankAccountDto account1DtoTest;
-  private BankAccountDto account2DtoTest;
 
   @BeforeEach
   void setUp() {
     userTest = new User(1,"test","test","test@mail.com","12345678", BigDecimal.ZERO, new Role(1,"USER"), new HashSet<>());
+    userTest.getBankAccounts().add(new BankAccount(1,"PrimaryAccount", "1234567890abcdefghijklmnopqrstu123","12345678abc",BigDecimal.valueOf(100)));
     account1DtoTest = new BankAccountDto(1, "PrimaryAccount","XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX123","XXXXXXXXabc");
-    account2DtoTest = new BankAccountDto(1, "PrimaryAccount","XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX456","XXXXXXXXdef");
   }
 
   @Test
   void getAllByUserIdTest() throws Exception {
     // GIVEN
-    userTest.getBankAccounts().add(new BankAccount(1,"PrimaryAccount", "1234567890abcdefghijklmnopqrstu123","12345678abc",BigDecimal.valueOf(100)));
-    userTest.getBankAccounts().add(new BankAccount(1,"PrimaryAccount", "1234567890abcdefghijklmnopqrstu456","12345678def",BigDecimal.valueOf(150)));
     when(userRepository.findById(anyInt())).thenReturn(Optional.of(userTest));
 
     // WHEN
     List<BankAccountDto> actualListBankAccountDto = bankAccountService.getAllByUserId(1);
 
     // THEN
-    assertThat(actualListBankAccountDto).usingRecursiveComparison().isEqualTo(List.of(account1DtoTest,account2DtoTest));
+    assertThat(actualListBankAccountDto).usingRecursiveComparison().isEqualTo(List.of(account1DtoTest));
     verify(userRepository, times(1)).findById(1);
   }
 
   @Test
   void getAllByUserIdWhenEmptyTest() throws Exception {
     // GIVEN
+    userTest.setBankAccounts(new HashSet<>());
     when(userRepository.findById(anyInt())).thenReturn(Optional.of(userTest));
 
     // WHEN
@@ -76,7 +72,7 @@ class BankAccountServiceTest {
   }
 
   @Test
-  void getAllByUserIdWhenNotFoundTest() {
+  void getAllByUserIdWhenUserNotFoundTest() {
     // GIVEN
     when(userRepository.findById(anyInt())).thenReturn(Optional.empty());
 
@@ -92,9 +88,11 @@ class BankAccountServiceTest {
   @Test
   void addToUserIdTest() throws Exception {
     // GIVEN
-    BankAccountDto accountToAddDto = new BankAccountDto(1, "PrimaryAccount","1234567890abcdefghijklmnopqrstu123","12345678xyz");
+    BankAccountDto accountToAddDto = new BankAccountDto(2, "PrimaryAccount","1234567890abcdefghijklmnopqrstu456","12345678xyz");
+    BankAccountDto maskedAccountDto = new BankAccountDto(2, "PrimaryAccount","XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX456","XXXXXXXXxyz");
     User updateUserTest = new User(1,"test","test","test@mail.com","12345678", BigDecimal.ZERO, new Role(1,"USER"), new HashSet<>());
     updateUserTest.getBankAccounts().add(new BankAccount(1,"PrimaryAccount", "1234567890abcdefghijklmnopqrstu123","12345678abc",BigDecimal.valueOf(100)));
+    updateUserTest.getBankAccounts().add(new BankAccount(2,"PrimaryAccount", "1234567890abcdefghijklmnopqrstu456","12345678xyz",BigDecimal.valueOf(100)));
     when(userRepository.findById(anyInt())).thenReturn(Optional.of(userTest));
     when(userRepository.save(any(User.class))).thenReturn(updateUserTest);
 
@@ -102,13 +100,13 @@ class BankAccountServiceTest {
     List<BankAccountDto> actualListBankAccountDto = bankAccountService.addToUserId(1,accountToAddDto);
 
     // THEN
-    assertThat(actualListBankAccountDto).usingRecursiveComparison().isEqualTo(List.of(account1DtoTest));
+    assertThat(actualListBankAccountDto).usingRecursiveFieldByFieldElementComparator().containsExactlyInAnyOrder(maskedAccountDto,account1DtoTest);
     verify(userRepository, times(1)).findById(1);
     verify(userRepository,times(1)).save(any(User.class));
   }
 
   @Test
-  void addToUserIdWhenNotFoundTest() {
+  void addToUserIdWhenUserNotFoundTest() {
     // GIVEN
     BankAccountDto accountToAddDto = new BankAccountDto(0, "PrimaryAccount","1234567890abcedfghijklmnopqrst789","12345678xyz");
     when(userRepository.findById(anyInt())).thenReturn(Optional.empty());
@@ -120,6 +118,52 @@ class BankAccountServiceTest {
         .isInstanceOf(ResourceNotFoundException.class)
         .hasMessageContaining("This user is not found");
     verify(userRepository, times(1)).findById(2);
+    verify(userRepository,times(0)).save(any(User.class));
+  }
+
+  @Test
+  void deleteByIdTest() throws Exception {
+    // GIVEN
+    User updateUserTest = new User(1,"test","test","test@mail.com","12345678", BigDecimal.ZERO, new Role(1,"USER"), new HashSet<>());
+    when(userRepository.findById(anyInt())).thenReturn(Optional.of(userTest));
+    when(userRepository.save(any(User.class))).thenReturn(updateUserTest);
+
+    // WHEN
+    List<BankAccountDto> actualListBankAccountDto = bankAccountService.deleteById(1,1);
+
+    // THEN
+    assertThat(actualListBankAccountDto).usingRecursiveComparison().isEqualTo(new ArrayList<>());
+    verify(userRepository, times(1)).findById(1);
+    verify(userRepository,times(1)).save(any(User.class));
+  }
+
+  @Test
+  void deleteByIdWhenUserNotFoundTest() {
+    // GIVEN
+    when(userRepository.findById(anyInt())).thenReturn(Optional.empty());
+
+    // WHEN
+    assertThatThrownBy(() -> bankAccountService.deleteById(2,1))
+
+        // THEN
+        .isInstanceOf(ResourceNotFoundException.class)
+        .hasMessageContaining("This user is not found");
+    verify(userRepository, times(1)).findById(2);
+    verify(userRepository,times(0)).save(any(User.class));
+  }
+
+  @Test
+  void deleteByIdWhenAccountNotFoundTest() {
+    // GIVEN
+    when(userRepository.findById(anyInt())).thenReturn(Optional.of(userTest));
+
+    // WHEN
+    assertThatThrownBy(() -> bankAccountService.deleteById(1,2))
+
+        // THEN
+        .isInstanceOf(ResourceNotFoundException.class)
+        .hasMessageContaining("This account is not found");
+    verify(userRepository, times(1)).findById(1);
     verify(userRepository,times(0)).save(any(User.class));
   }
 
