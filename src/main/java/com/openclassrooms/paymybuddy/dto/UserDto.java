b3d12866395e8fc24c3a4628e332296c5a263311
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-07 16:54:28 +0100

    Merge UserInfoDto and UserTegistrationDto into UserDto

--- src/main/java/com/openclassrooms/paymybuddy/dto/UserDto.java
+++ src/main/java/com/openclassrooms/paymybuddy/dto/UserDto.java
@@ -1,8 +1,10 @@
 package com.openclassrooms.paymybuddy.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.math.BigDecimal;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +17,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserInfoDto {
+public class UserDto {
 
   private int userId;
   @NotBlank(message = "Firstname is mandatory")
@@ -25,7 +27,13 @@ public class UserInfoDto {
   @NotBlank(message = "Email is mandatory")
   @Email(message = "Email should be a valid email address")
   private String email;
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  @NotBlank(message = "Password is mandatory")
+  @Size(min = 8, message = "Password should have at least 8 characters")
+  private String password;
+  @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   private BigDecimal wallet;
+  @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   private String role;
 
 }
