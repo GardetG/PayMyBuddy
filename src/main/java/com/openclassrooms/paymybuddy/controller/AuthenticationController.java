e6bb4fe9f581a7bcbe61dd03cfa95777bcad6226
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-07 19:25:57 +0100

    Implement AuthenticationController login() method

--- src/main/java/com/openclassrooms/paymybuddy/controller/AuthenticationController.java
+++ src/main/java/com/openclassrooms/paymybuddy/controller/AuthenticationController.java
@@ -2,14 +2,19 @@ package com.openclassrooms.paymybuddy.controller;
 
 import com.openclassrooms.paymybuddy.dto.UserDto;
 import com.openclassrooms.paymybuddy.exception.ResourceAlreadyExistsException;
+import com.openclassrooms.paymybuddy.model.User;
 import com.openclassrooms.paymybuddy.service.UserService;
+import com.openclassrooms.paymybuddy.utils.UserMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -42,5 +47,22 @@ public class AuthenticationController {
 
     LOGGER.info("Response: User successfully registered");
     return ResponseEntity.status(HttpStatus.CREATED).body(userInfo);
+  }
+
+  /**
+   * Handle HTTP GET user login.
+   *
+   * @param myUser user authenticate
+   * @return User information
+   */
+  @PreAuthorize("hasAnyRole('USER','ADMIN')")
+  @GetMapping("/login")
+  public ResponseEntity<UserDto> login(@AuthenticationPrincipal User myUser) {
+
+    LOGGER.info("Request: Login user {}", myUser.getUserId());
+    UserDto userInfo = UserMapper.toInfoDto(myUser);
+
+    LOGGER.info("Response: User information sent");
+    return ResponseEntity.ok(userInfo);
   }
 }
