b646296cea603be68715a14af28149e0606848e7
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-07 18:56:37 +0100

    Add AuthenticationController POST subscribe() unit tests

--- src/test/java/com/openclassrooms/paymybuddy/controller/UserControllerTest.java
+++ src/test/java/com/openclassrooms/paymybuddy/controller/UserControllerTest.java
@@ -10,25 +10,23 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.openclassrooms.paymybuddy.dto.UserInfoDto;
-import com.openclassrooms.paymybuddy.dto.UserRegistrationDto;
+import com.openclassrooms.paymybuddy.dto.UserDto;
+import com.openclassrooms.paymybuddy.exception.ForbbidenOperationException;
 import com.openclassrooms.paymybuddy.exception.ResourceAlreadyExistsException;
 import com.openclassrooms.paymybuddy.exception.ResourceNotFoundException;
 import com.openclassrooms.paymybuddy.model.Role;
 import com.openclassrooms.paymybuddy.model.User;
 import com.openclassrooms.paymybuddy.service.CredentialsService;
 import com.openclassrooms.paymybuddy.service.UserService;
-import com.openclassrooms.paymybuddy.utils.JsonParser;
 import java.math.BigDecimal;
 import java.util.List;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -55,20 +53,20 @@ class UserControllerTest {
   private CredentialsService credentialsService;
 
   @Captor
-  ArgumentCaptor<UserRegistrationDto> subscriptionCaptor;
-  @Captor
-  ArgumentCaptor<UserInfoDto> infoCaptor;
+  ArgumentCaptor<UserDto> infoCaptor;
 
-  private UserInfoDto userInfoDto;
+  private UserDto userInfoDto;
   private User userTest;
   private User adminTest;
+  private JSONObject jsonParam;
 
   @BeforeEach
   void setUp() {
-    userInfoDto = new UserInfoDto(1, "test","test","test@mail.com", BigDecimal.ZERO, "USER");
+    userInfoDto = new UserDto(1, "test","test","test@mail.com",null, BigDecimal.ZERO, "USER");
     userTest = new User("test","test","user1@mail.com","password", Role.USER);
     userTest.setUserId(1);
     adminTest = new User("test","test","test@mail.com","password", Role.ADMIN);
+    jsonParam = new JSONObject();
   }
 
   @Test
@@ -113,7 +111,9 @@ class UserControllerTest {
         .andExpect(jsonPath("$.firstname", is("test")))
         .andExpect(jsonPath("$.lastname", is("test")))
         .andExpect(jsonPath("$.email", is("test@mail.com")))
-        .andExpect(jsonPath("$.wallet", is(0)));
+        .andExpect(jsonPath("$.password").doesNotExist())
+        .andExpect(jsonPath("$.wallet", is(0)))
+        .andExpect(jsonPath("$.role", is("USER")));
     verify(userService, times(1)).getById(1);
   }
 
@@ -161,57 +161,65 @@ class UserControllerTest {
   @Test
   void putUpdateTest() throws Exception {
     // GIVEN
-    UserInfoDto updateDto = new UserInfoDto(1,"update", "test", "update@mail.com", BigDecimal.ZERO, "USER");
-    when(userService.update(any(UserInfoDto.class))).thenReturn(updateDto);
+    jsonParam.put("userId",1).put("firstname","update")
+        .put("lastname","test").put("email","new@mail.com");
+    UserDto updateDto = new UserDto(1,"update", "test", "new@mail.com", null,BigDecimal.ZERO, "USER");
+    when(userService.update(any(UserDto.class))).thenReturn(updateDto);
 
     // WHEN
     mockMvc.perform(put("/users").with(user(userTest))
             .with(csrf())
             .contentType(MediaType.APPLICATION_JSON)
-            .content(JsonParser.asString(updateDto)))
+            .content(jsonParam.toString()))
 
         // THEN
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.userId", is(1)))
         .andExpect(jsonPath("$.firstname", is("update")))
         .andExpect(jsonPath("$.lastname", is("test")))
-        .andExpect(jsonPath("$.email", is("update@mail.com")))
-        .andExpect(jsonPath("$.wallet", is(0)));
+        .andExpect(jsonPath("$.email", is("new@mail.com")))
+        .andExpect(jsonPath("$.wallet", is(0)))
+        .andExpect(jsonPath("$.email", is("new@mail.com")))
+        .andExpect(jsonPath("$.password").doesNotExist())
+        .andExpect(jsonPath("$.role", is("USER")));
     verify(userService, times(1)).update(infoCaptor.capture());
-    assertThat(infoCaptor.getValue()).usingRecursiveComparison().isEqualTo(updateDto);
-
+    UserDto expected = new UserDto(1,"update", "test", "new@mail.com", null,null, null);
+    assertThat(infoCaptor.getValue()).usingRecursiveComparison().isEqualTo(expected);
   }
 
   @Test
   void putUpdateWithAlreadyUsedEmailTest() throws Exception {
     // GIVEN
-    UserInfoDto updateDto = new UserInfoDto(1,"update", "test", "existing@mail.com", BigDecimal.ZERO, "USER");
-    when(userService.update(any(UserInfoDto.class))).thenThrow(
+    jsonParam.put("userId",1).put("firstname","update")
+        .put("lastname","test").put("email","existing@mail.com");
+    when(userService.update(any(UserDto.class))).thenThrow(
         new ResourceAlreadyExistsException("This email is already used"));
 
     // WHEN
     mockMvc.perform(put("/users").with(user(userTest))
             .with(csrf())
             .contentType(MediaType.APPLICATION_JSON)
-            .content(JsonParser.asString(updateDto)))
+            .content(jsonParam.toString()))
 
         // THEN
         .andExpect(status().isConflict())
         .andExpect(jsonPath("$", is("This email is already used")));
     verify(userService, times(1)).update(infoCaptor.capture());
-    assertThat(infoCaptor.getValue()).usingRecursiveComparison().isEqualTo(updateDto);
+    UserDto expected = new UserDto(1,"update", "test", "existing@mail.com", null,null, null);
+    assertThat(infoCaptor.getValue()).usingRecursiveComparison().isEqualTo(expected);
   }
 
   @Test
   void putUpdateWhenAuthenticateButIdNotMatchingTest() throws Exception {
     // GIVEN
-    UserInfoDto updateDto = new UserInfoDto(2,"update", "test", "update@mail.com", BigDecimal.ZERO, "USER");
+    jsonParam.put("userId",9).put("firstname","update")
+        .put("lastname","test").put("email","update@mail.com");
 
     // WHEN
     mockMvc.perform(put("/users").with(user(userTest))
             .with(csrf())
             .contentType(MediaType.APPLICATION_JSON)
-            .content(JsonParser.asString(updateDto)))
+            .content(jsonParam.toString()))
 
         // THEN
         .andExpect(status().isForbidden());
@@ -221,31 +229,35 @@ class UserControllerTest {
   @Test
   void putInvalidUpdateTest() throws Exception {
     // GIVEN
-    UserInfoDto invalidUpdateDto = new UserInfoDto(1,"","test", "testmail.com", BigDecimal.ZERO, "USER");
+    jsonParam.put("userId",1).put("firstname","")
+        .put("  ","test").put("email","mail.com").put("password","123");
 
     // WHEN
     mockMvc.perform(put("/users").with(user(userTest))
             .with(csrf())
             .contentType(MediaType.APPLICATION_JSON)
-            .content(JsonParser.asString(invalidUpdateDto)))
+            .content(jsonParam.toString()))
 
         // THEN
         .andExpect(status().isUnprocessableEntity())
         .andExpect(jsonPath("$.firstname", is("Firstname is mandatory")))
-        .andExpect(jsonPath("$.email", is("Email should be a valid email address")));
-    verify(userService, times(0)).register(any(UserRegistrationDto.class));
+        .andExpect(jsonPath("$.lastname", is("Lastname is mandatory")))
+        .andExpect(jsonPath("$.email", is("Email should be a valid email address")))
+        .andExpect(jsonPath("$.password", is("Password should have at least 8 characters")));
+    verify(userService, times(0)).register(any(UserDto.class));
   }
 
   @Test
   void putUpdateWhenNotAuthenticateTest() throws Exception {
     // GIVEN
-    UserInfoDto updateDto = new UserInfoDto(1,"update", "test", "update@mail.com", BigDecimal.ZERO, "USER");
+    jsonParam.put("userId",1).put("firstname","update")
+        .put("lastname","test").put("email","new@mail.com");
 
     // WHEN
     mockMvc.perform(put("/users")
             .with(csrf())
             .contentType(MediaType.APPLICATION_JSON)
-            .content(JsonParser.asString(updateDto)))
+            .content(jsonParam.toString()))
 
         // THEN
         .andExpect(status().isUnauthorized());
@@ -260,6 +272,36 @@ class UserControllerTest {
 
         // THEN
         .andExpect(status().isNoContent());
+    verify(userService, times(1)).deleteById(1);
+  }
+
+  @Test
+  void deleteUserWithUserNotFoundTest() throws Exception {
+    // GIVEN
+    doThrow(new ResourceNotFoundException("This user is not found")).when(userService)
+        .deleteById(anyInt());
+
+    // WHEN
+    mockMvc.perform(delete("/users/9").with(user(adminTest)))
+
+        // THEN
+        .andExpect(status().isNotFound())
+        .andExpect(jsonPath("$", is("This user is not found")));
+    verify(userService, times(1)).deleteById(9);
+  }
+
+  @Test
+  void deleteUserWithNotEmptyWalletTest() throws Exception {
+    // GIVEN
+    doThrow(new ForbbidenOperationException("Can't delete user if wallet not empty")).when(userService)
+        .deleteById(anyInt());
+
+    // WHEN
+    mockMvc.perform(delete("/users/1").with(user(userTest)))
+
+        // THEN
+        .andExpect(status().isConflict())
+        .andExpect(jsonPath("$", is("Can't delete user if wallet not empty")));
     verify(userService, times(1)).deleteById(1);
   }
 
