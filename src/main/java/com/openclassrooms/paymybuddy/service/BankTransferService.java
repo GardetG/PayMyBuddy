e7ac551c5e92032c654067bfd9a298d1ef17d6ee
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-08 00:59:06 +0100

    Add BankService

---
+++ src/main/java/com/openclassrooms/paymybuddy/service/BankTransferService.java
@@ -0,0 +1,10 @@
+package com.openclassrooms.paymybuddy.service;
+
+/**
+ * Service Interface for managing bank transfer between bank account and user wallet.
+ */
+public interface BankTransferService {
+
+
+
+}
