c249a9ff86b6927ff2b67377eae5906dcdf426f7
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-08 17:58:22 +0100

    Add TransactionService

---
+++ src/main/java/com/openclassrooms/paymybuddy/service/TransactionServiceImpl.java
@@ -0,0 +1,45 @@
+package com.openclassrooms.paymybuddy.service;
+
+import com.openclassrooms.paymybuddy.dto.TransactionDto;
+import com.openclassrooms.paymybuddy.exception.InsufficientProvisionException;
+import com.openclassrooms.paymybuddy.exception.ResourceNotFoundException;
+import com.openclassrooms.paymybuddy.repository.TransactionRepository;
+import com.openclassrooms.paymybuddy.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service Class for managing bank transfer between bank account and user wallet.
+ */
+@Service
+public class TransactionServiceImpl implements TransactionService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(TransactionServiceImpl.class);
+
+  @Autowired
+  TransactionRepository transactionRepository;
+
+  @Autowired
+  UserRepository userRepository;
+
+  @Override
+  public Page<TransactionDto> getAll(Pageable pageable) {
+    return null;
+  }
+
+  @Override
+  public Page<TransactionDto> getFromUser(int userId, Pageable pageable)
+      throws ResourceNotFoundException {
+    return null;
+  }
+
+  @Override
+  public TransactionDto requestTansaction(TransactionDto request)
+      throws ResourceNotFoundException, InsufficientProvisionException {
+    return null;
+  }
+}
