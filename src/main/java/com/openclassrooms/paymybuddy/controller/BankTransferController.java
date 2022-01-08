140b69d13078ac41036fa660c1666dc5dbf2900d
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-08 15:53:00 +0100

    Implement BankTransferController request() method

--- src/main/java/com/openclassrooms/paymybuddy/controller/BankTransferController.java
+++ src/main/java/com/openclassrooms/paymybuddy/controller/BankTransferController.java
@@ -1,18 +1,23 @@
 package com.openclassrooms.paymybuddy.controller;
 
 import com.openclassrooms.paymybuddy.dto.BankTransferDto;
+import com.openclassrooms.paymybuddy.exception.InsufficientProvisionException;
 import com.openclassrooms.paymybuddy.exception.ResourceNotFoundException;
 import com.openclassrooms.paymybuddy.service.BankTransferService;
+import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -64,4 +69,23 @@ public class BankTransferController {
     return ResponseEntity.ok(bankTransfersDto);
   }
 
+  /**
+   * Handle HTTP POST request for a bank transfer.
+   *
+   * @param request of the transfer
+   * @return HTTP Response 201 with transfer executed
+   * @throws InsufficientProvisionException if provision insufficient to perform transfer
+   * @throws ResourceNotFoundException if account not found
+   */
+  @PreAuthorize("#request.userId == authentication.principal.userId")
+  @PostMapping("/banktransfers")
+  public ResponseEntity<BankTransferDto> request(@Valid @RequestBody BankTransferDto request)
+      throws InsufficientProvisionException, ResourceNotFoundException {
+
+    LOGGER.info("Request: Bank transfer for user {} with account {}", request.getUserId(), request.getBankAccountId());
+    BankTransferDto requestResponse = bankTransferService.requestTransfer(request);
+
+    LOGGER.info("Response: Bank transfer successfully executed");
+    return ResponseEntity.status(HttpStatus.CREATED).body(requestResponse);
+  }
 }
