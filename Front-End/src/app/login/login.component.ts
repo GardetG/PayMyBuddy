a3086b187f282c96fea460850a70aaedba5c2db4
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-09 14:11:23 +0100

    Add LoginComponent HTML formular and doLogin()

--- Front-End/src/app/login/login.component.ts
+++ Front-End/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email:string = "";
+  password:string = "";
+
+  constructor(private service:AuthenticationService) { }
 
   ngOnInit(): void {
+  }
+
+  doLogin() {
+    let resp = this.service.login(this.email, this.password);
+    resp.subscribe((data:any) => {
+      console.log(data)
+    })
   }
 
 }
