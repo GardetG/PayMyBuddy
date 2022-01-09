5831643f3e310b5a7768975d1b8da8a86af0b80a
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-09 18:55:08 +0100

    Retrieve Identity in AuthenticationService

--- Front-End/src/app/service/authentication.service.ts
+++ Front-End/src/app/service/authentication.service.ts
@@ -1,15 +1,23 @@
 import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { Identity } from '../model/Identity/identity.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
+  public identity!: Identity;
+
   constructor(private http:HttpClient) { }
 
   public login(email:string, password:string) {
     const headers = new HttpHeaders({Authorization: 'Basic ' + btoa(email+":"+password)});
-    return this.http.get("http://localhost:8080/login", {headers, responseType:'text' as 'json'});
+    return this.http.get<Identity>("http://localhost:8080/login", {headers})
+    .pipe(map(resp => {
+      this.identity = resp;
+      return resp;
+    }))
   }
 }
