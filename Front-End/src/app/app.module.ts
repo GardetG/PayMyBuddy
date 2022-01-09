a5a8f86d1c7af1b39d197a9e7996f9076af01bb0
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-09 14:12:10 +0100

    Set default route to LoginComponent

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationService } from './authentication.service';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
@@ -12,9 +15,11 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
