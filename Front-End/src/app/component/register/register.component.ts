2845da5e61dd5d4f7a970ccf4deaeeeaf2174f26
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-13 15:48:55 +0100

    Implement confirmpassword validator

--- Front-End/src/app/component/register/register.component.ts
+++ Front-End/src/app/component/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/service/authentication.service';
+import { MustMatch } from 'src/app/Validator/confirmpassword.validator';
 
 @Component({
   selector: 'app-register',
@@ -15,16 +16,22 @@ export class RegisterComponent implements OnInit {
     "firstname": ["", Validators.required],
     "lastname": ["", Validators.required],
     "email": ["", Validators.required],
-    "password": ["", Validators.required],
+    "password": ["", [Validators.required, Validators.minLength(8)]],
     "confirmpassword": [""],
     "termsAgreement": [false, Validators.requiredTrue],
     "privacyAgreement": [false, Validators.requiredTrue],
+  }, {
+    validator: MustMatch('password', 'confirmpassword')
   });
 
   constructor(private auth:AuthenticationService,private router:Router,private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
+
+  get getControls() {
+    return this.form.controls;
+}
 
   get firstname() {
     return this.form.get('firstname');
