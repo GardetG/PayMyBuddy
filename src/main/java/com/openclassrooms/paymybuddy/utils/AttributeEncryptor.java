04f7d7728c3aad10efd5a7ea5c80643ae47d1b5e
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-20 14:46:36 +0100

    Pass DB credentials and crypt keyword as argument of the application

--- src/main/java/com/openclassrooms/paymybuddy/utils/AttributeEncryptor.java
+++ src/main/java/com/openclassrooms/paymybuddy/utils/AttributeEncryptor.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.paymybuddy.utils;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
@@ -10,6 +12,7 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 import javax.persistence.AttributeConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -18,14 +21,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class AttributeEncryptor implements AttributeConverter<String, String> {
 
-  private static final String AES = "AES/GCM/NoPadding";
-  private static final String SECRET = "paymybuddy_crypt";
+  private static final String AES = "AES";
+  private static final Charset CHARSET = StandardCharsets.UTF_8;
 
   private final Key key;
   private final Cipher cipher;
 
-  public AttributeEncryptor() throws NoSuchPaddingException, NoSuchAlgorithmException {
-    key = new SecretKeySpec(SECRET.getBytes(), AES);
+  public AttributeEncryptor(@Value("${PayMyBuddy.crypt.keyword}") String keyword)
+      throws NoSuchPaddingException, NoSuchAlgorithmException {
+    key = new SecretKeySpec(keyword.getBytes(CHARSET), AES);
     cipher = Cipher.getInstance(AES);
   }
 
@@ -33,7 +37,7 @@ public class AttributeEncryptor implements AttributeConverter<String, String> {
   public String convertToDatabaseColumn(String attribute) {
     try {
       cipher.init(Cipher.ENCRYPT_MODE, key);
-      return Base64.getEncoder().encodeToString(cipher.doFinal(attribute.getBytes()));
+      return Base64.getEncoder().encodeToString(cipher.doFinal(attribute.getBytes(CHARSET)));
     } catch (IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
       throw new IllegalStateException(e);
     }
@@ -43,7 +47,7 @@ public class AttributeEncryptor implements AttributeConverter<String, String> {
   public String convertToEntityAttribute(String dbData) {
     try {
       cipher.init(Cipher.DECRYPT_MODE, key);
-      return new String(cipher.doFinal(Base64.getDecoder().decode(dbData)));
+      return new String(cipher.doFinal(Base64.getDecoder().decode(dbData)), CHARSET);
     } catch (InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
       throw new IllegalStateException(e);
     }
