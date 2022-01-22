6071c9878f9c7078d3a84eeb2a51de4e2e6567fe
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-23 00:32:46 +0100

    Update ApiPayMyBuddy to handle transactions end-points

--- Front-End/src/app/service/api-paymybuddy.service.ts
+++ Front-End/src/app/service/api-paymybuddy.service.ts
@@ -4,6 +4,7 @@ import { map, Observable } from 'rxjs';
 import { BankAccount } from '../model/BankAccount/bank-account.model';
 import { BankTransfer } from '../model/BankTransfer/bank-transfer.model';
 import { Connection } from '../model/Connection/connection.model';
+import { Transaction } from '../model/Transaction/transaction.model';
 import { User } from '../model/User/user.model';
 import { AuthenticationService } from './authentication.service';
 
@@ -19,6 +20,7 @@ export class ApiPaymybuddyService {
   }
 
   // Connection end-points
+
   public getAllConnections(): Observable<Connection[]> {
     return this.http.get<any>("http://localhost:8080/users/" + this.auth.getIdentity().userId + "/connections")
     .pipe(map(page => {
@@ -40,6 +42,7 @@ export class ApiPaymybuddyService {
   }
 
   // Bank account end-points
+
   public getAllBankAccounts(): Observable<BankAccount[]> {
     return this.http.get<any>("http://localhost:8080/users/" + this.auth.getIdentity().userId + "/bankaccounts")
     .pipe(map(page => {
@@ -67,8 +70,20 @@ export class ApiPaymybuddyService {
     return this.http.get<any>("http://localhost:8080/banktransfers/user?id=" + this.auth.getIdentity().userId + pageable)
   }
 
-  public requestBankTransfer(transfer:BankTransfer): Observable<BankTransfer> {
-    transfer.userId = this.auth.getIdentity().userId;
-    return this.http.post<BankTransfer>("http://localhost:8080/banktransfers",transfer)
+  public requestBankTransfer(request:BankTransfer): Observable<BankTransfer> {
+    request.userId = this.auth.getIdentity().userId;
+    return this.http.post<BankTransfer>("http://localhost:8080/banktransfers", request)
   }
+
+    // Transaction end-points
+
+    public getPageOfTransactions(page:number, size:number): Observable<any> {
+      let pageable = "&page=" + page + "&size=" + size + "&sort=date,desc"
+      return this.http.get<any>("http://localhost:8080/transactions/user?id=" + this.auth.getIdentity().userId + pageable)
+    }
+  
+    public requestTransaction(request:Transaction): Observable<Transaction> {
+      request.emitterId = this.auth.getIdentity().userId;
+      return this.http.post<Transaction>("http://localhost:8080/transactions", request)
+    }
 }
