0b1abfca9f75d1e6b6bbd1532749a7e6ad7f52f4
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-22 20:24:25 +0100

    Add BankTransfer Model

---
+++ Front-End/src/app/model/BankTransfer/bank-transfer.model.spec.ts
@@ -0,0 +1,7 @@
+import { BankTransfer } from './bank-transfer.model';
+
+describe('BankTransfer', () => {
+  it('should create an instance', () => {
+    expect(new BankTransfer()).toBeTruthy();
+  });
+});
---
+++ Front-End/src/app/model/BankTransfer/bank-transfer.model.ts
@@ -0,0 +1,10 @@
+export class BankTransfer {
+    userId:number = 0;
+    bankAccountId:number = 0;
+    amount:number = 0;
+    isIncome:boolean = true;
+    date:string = "";
+    firstname:string = "";
+    lastname:string = "";
+    title:string = "";
+}
