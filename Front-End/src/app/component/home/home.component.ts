d8fbb82db5fb2220bbbf2e4969faaa96fb68977b
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-01-24 07:20:12 +0100

    Add BreadCrumb to HomeComponent

--- Front-End/src/app/component/home/home.component.ts
+++ Front-End/src/app/component/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
+import { Breadcrumb } from 'src/app/model/Breadcrumb/breadcrumb.model';
 import { AuthenticationService } from 'src/app/service/authentication.service';
 
 @Component({
@@ -10,20 +11,38 @@ import { AuthenticationService } from 'src/app/service/authentication.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private auth:AuthenticationService ,private activatedRoute: ActivatedRoute, private router: Router) { }
+  breadcrumbs:Breadcrumb[] = [];
+  currentRoute:string=""
+
+  constructor(private auth:AuthenticationService , private router: Router) { }
 
   ngOnInit(): void {
+    this.parseRoute(this.router.url);
     this.router.events.pipe
       (filter(value => value instanceof NavigationEnd)).subscribe((value: any) => {
-          console.log(this.router.url) // ['home']
+          this.parseRoute(this.router.url);
       });
+  }
+
+  parseRoute(url:string) {
+    let elements:string[] = url.substring(1).split('/')
+    .map(element => element[0].toUpperCase() + element.slice(1));
+
+    this.breadcrumbs = elements.slice(0,elements.length-1)
+    .map(function callback(value, index, array) {
+      let breadcrumb:Breadcrumb = new Breadcrumb();
+      breadcrumb.label = value;
+      breadcrumb.url = './' + array.slice(1, index+1).join("/");
+      return breadcrumb;
+    });
+
+    this.currentRoute = elements.pop()!;
   }
 
   doLogOff() {
     this.auth.logoff()
     .subscribe({
       next: (v) => {
-        console.log(v);
         this.router.navigate(["login"]);
       },
       error: (e) => {
